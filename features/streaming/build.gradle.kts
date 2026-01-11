c3af0844c8f4779ca95588634727c41fd282b7aa
Zash60 <xath06@gmail.com>
2026-01-11 14:54:32 -0300

    Update build.gradle.kts

--- features/streaming/build.gradle.kts
+++ features/streaming/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     // WorkManager
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("androidx.hilt:hilt-work:1.0.0")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
 
     // Room - Runtime only, no compiler needed here as entities are in :data
     implementation("androidx.room:room-runtime:2.6.1")
