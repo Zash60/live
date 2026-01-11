d71245863ee2b99c28e184f566f28ca9f3af874c
Zash60 <xath06@gmail.com>
2026-01-11 14:41:14 -0300

    Update StreamPreset.kt

--- features/streaming/src/main/java/com/example/liveapp/features/streaming/domain/model/StreamPreset.kt
+++ features/streaming/src/main/java/com/example/liveapp/features/streaming/domain/model/StreamPreset.kt
@@ -1,5 +1,7 @@
 package com.example.liveapp.features.streaming.domain.model
 
+import com.example.liveapp.domain.model.StreamConfig
+
 data class StreamPreset(
     val id: Long = 0,
     val name: String,
