026f88bd5b1eb3d674522fb38203e1f6b0044b4e
Zash60 <xath06@gmail.com>
2026-01-14 10:32:17 -0300

    Upgrade Kotlin compiler extension version
    Updated kotlinCompilerExtensionVersion from 1.5.0 to 1.5.1.

--- features/settings/build.gradle.kts
+++ features/settings/build.gradle.kts
@@ -31,7 +31,8 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        // CORREÇÃO: Atualizado para 1.5.1
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
