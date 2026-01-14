11303ad04a0ba1a431788c6b58d230036eba20fd
Zash60 <xath06@gmail.com>
2026-01-14 08:00:10 -0300

    Add dependency injection to StartStreamUseCase

--- features/streaming/src/main/java/com/example/liveapp/features/streaming/domain/usecase/StartStreamUseCase.kt
+++ features/streaming/src/main/java/com/example/liveapp/features/streaming/domain/usecase/StartStreamUseCase.kt
@@ -2,9 +2,10 @@ package com.example.liveapp.features.streaming.domain.usecase
 
 import com.example.liveapp.domain.model.StreamConfig
 import com.example.liveapp.features.streaming.domain.repository.StreamingRepository
+import javax.inject.Inject
 
 class StartStreamUseCase @Inject constructor(
-    private val repository: StreamingRepository
+private val repository: StreamingRepository
 ) {
-    suspend operator fun invoke(config: StreamConfig) = repository.startStream(config)
+suspend operator fun invoke(config: StreamConfig) = repository.startStream(config)
 }
