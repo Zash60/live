da17fc9dbeb13917c3c1835632d277781b496e79
Zash60 <xath06@gmail.com>
2026-01-14 09:22:52 -0300

    Add material-icons-extended dependency to build.gradle.kts

--- features/settings/build.gradle.kts
+++ features/settings/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     // Compose
     implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     // Navigation
