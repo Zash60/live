5736418197ec18ad2aed63b2fcaa8cada6c4a8d2
Zash60 <xath06@gmail.com>
2026-01-14 10:56:19 -0300

    Add dependency injection to GetProfileUseCase

--- domain/src/main/java/com/example/liveapp/domain/usecase/GetProfileUseCase.kt
+++ domain/src/main/java/com/example/liveapp/domain/usecase/GetProfileUseCase.kt
@@ -3,8 +3,9 @@ package com.example.liveapp.domain.usecase
 import com.example.liveapp.domain.model.User
 import com.example.liveapp.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetProfileUseCase constructor(
+class GetProfileUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
     operator fun invoke(): Flow<Result<User>> = userRepository.getProfile()
